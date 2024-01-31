2b270b486ecebee7ab4381f9cae903bddc87bc69
Mihai Balan <mihai.balan@esolutions.ro>
2024-01-31 12:57:26 +0200

    + moment

--- src/components/ScheduleWidget.tsx
+++ src/components/ScheduleWidget.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { Key, useEffect, useState } from "react";
+import moment from "moment";
 
 type ScheduleData = {
   data: [
@@ -24,6 +25,9 @@ const ScheduleWidget = () => {
   const [data, setData] = useState<ScheduleData>();
   const url = "https://public.radio.co/stations/s1cdb8ef73/embed/schedule";
 
+  const [currentTime, setCurrentTime] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+
   const getData = () => {
     fetch(url)
       .then((response) => response.json())
@@ -31,6 +35,19 @@ const ScheduleWidget = () => {
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      var time = moment().format(" hh:mm A");
+      setCurrentTime(time);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    var date = moment().format("yyyy-MM-DD");
+    setCurrentDate(date);
+  }, []);
 
   useEffect(() => {
     getData();
@@ -42,7 +59,9 @@ const ScheduleWidget = () => {
         <div className="text-lg font-bold text-start tracking-tight text-gray-900 sm:text-xl">
           Schedule
         </div>
-        <div>19:03 GMT</div>
+        <div className="text-md font-semibold text-start tracking-tight text-gray-900">
+          {currentTime}
+        </div>
       </div>
 
       <div className="h-96 overflow-y-scroll no-scrollbar">
@@ -50,18 +69,30 @@ const ScheduleWidget = () => {
           <>loading</>
         ) : (
           data?.data.map((data) => (
-            <div
-              key={data.playlist?.id}
-              className="flex flex-row justify-between py-4 items-center"
-            >
-              <div className=" text-md font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                {data.start?.slice(11, -9)} - {data.end?.slice(11, -9)}
-              </div>
+            <>
+              {data.end &&
+              new Date(data.end.slice(0, -15)) >= new Date(currentDate) ? (
+                <div
+                  key={data.playlist?.id}
+                  className="flex flex-row justify-between py-4 items-center"
+                >
+                  <div className="flex-col">
+                    <div className=" text-md font-semibold text-gray-900  shrink-0">
+                      {moment(data.end?.slice(0, -15)).format("dddd")}
+                    </div>
+                    <div className=" text-md font-normal text-gray-500  dark:text-gray-400 shrink-0">
+                      {data.start?.slice(11, -9)} - {data.end?.slice(11, -9)}
+                    </div>
+                  </div>
 
-              <div className="text-md font-semibold text-right pr-2 text-gray-900 w-36">
-                {data.playlist?.title}
-              </div>
-            </div>
+                  <div className="text-md font-semibold text-right pr-2 text-gray-900 w-36">
+                    {data.playlist?.title}
+                  </div>
+                </div>
+              ) : (
+                <></>
+              )}
+            </>
           ))
         )}
       </div>
