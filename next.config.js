d9c9b273021206838436fe1ef053b6cc089c2209
Mihai Balan <mihai.balan@everymatrix.com>
2025-02-18 14:53:08 +0200

    supppabase

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['yreqwxgymltdrkksqxda.supabase.co'],
     remotePatterns: [
       {
         protocol: "https",
--- src/components/BannerWidget.tsx
+++ src/components/BannerWidget.tsx
@@ -1,16 +1,67 @@
 "use client";
 import Image from "next/image";
-import banner from "../../public/PI  (520 x 276 px) (402 x 340 px) (520 x 276 px) (402 x 340 px) (2).png";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { supabase } from "../app/supabaseClient";
 
+interface ImageData {
+  id: string;
+  imageUrl: string;
+  imageSrc: string;
+}
 
 export default function BannerWidget() {
+  const [error, setError] = useState<string | null>(null);
+  const [images, setImages] = useState<ImageData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      setLoading(true); // Set loading true when starting to fetch
+      const { data, error } = await supabase
+        .from("banners")
+        .select("*")
+        .order("id", { ascending: false });
+
+      if (error) {
+        console.error("Fetch Error:", error.message);
+        setError(error.message); // Set error message if fetch fails
+      } else {
+        setImages(data || []); // Set images if fetch is successful
+      }
+      setLoading(false); // Set loading to false after fetch
+    };
+
+    fetchImages();
+  }, []);
 
   return (
     <div className="h-auto">
-      <Link href='https://romanian.levenes.co.uk/' target="_blank">
-        <Image src={banner} alt="" priority height={276} width={520} className="rounded-lg shadow-md w-full" />
-      </Link>
+      {loading ? (
+        <div className="w-full h-full flex items-center justify-center">
+          <p>Loading ad...</p>
+        </div>
+      ) : error ? (
+        <div className="w-full h-full bg-red-600 p-6 rounded-lg flex items-center justify-center text-white">
+          <p>Error: {error}</p>
+        </div>
+      ) : images.length === 0 ? (
+        <div className="w-full h-full bg-red-600 p-6 h-full rounded-lg flex items-center justify-center text-white">
+        </div>
+      ) : (
+              <Link href={images[0].imageUrl} target="_blank">
+                <Image
+                  src={images[0].imageSrc}
+                  alt="Banner"
+                  priority
+                  layout="intrinsic"
+                  height={276}
+                  width={520}
+                  className="rounded-lg shadow-md w-full h-auto object-cover"
+                  onError={() => setError("Failed to load image")}
+                />
+              </Link>
+      )}
     </div>
   );
 }
